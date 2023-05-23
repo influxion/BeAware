08689976d37574dbd7fb552b5b2d2d436424e4d2
Jordon Nichols <nicholsjordon721@gmail.com>
2023-05-23 17:57:35 -0500

    minor mobile fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
 
   return (
     <div className="max-w-5xl mx-auto px-4">
-      <div className="relative mt-72 mb-48 md:w-1/2 flex flex-col gap-2 before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+      <div className="relative md:mt-72 mt-36 md:mb-48 mb-24 md:w-1/2 flex flex-col gap-2 before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <Title className="!mb-0">Be Aware!</Title>
         <p className="dark:text-white/75 text-black/75">
           As we all know the vastness of cybersecurity can be overwhelming. Here
