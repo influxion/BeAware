7f3e07ec52c6bbd74700698b36b42675c70f07c7
Jordon Nichols <nicholsjordon721@gmail.com>
2023-05-23 18:03:52 -0500

    minor ui fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,8 @@ export default async function Home() {
 
   return (
     <div className="max-w-5xl mx-auto px-4">
-      <div className="relative md:mt-72 mt-36 md:mb-48 mb-24 md:w-1/2 flex flex-col gap-2 before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Title className="!mb-0">Be Aware!</Title>
+      <div className="relative md:mt-72 mt-48 md:mb-48 mb-24 md:w-1/2 flex flex-col gap-2 before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+        <Title className="!m-0">Be Aware!</Title>
         <p className="dark:text-white/75 text-black/75">
           As we all know the vastness of cybersecurity can be overwhelming. Here
           you will find resources to help protect youself or your business from
@@ -24,7 +24,7 @@ export default async function Home() {
         </p>
         <Link
           href="/archives"
-          className="py-2 px-4 w-fit mt-2 rounded-md dark:bg-white dark:text-black text-white bg-black z-10 hover:scale-105 transition-all duration-300 ease-in-out focus:scale-105"
+          className="py-2 px-4 w-fit mt-4 rounded-md dark:bg-white dark:text-black text-white bg-black z-10 hover:scale-105 transition-all duration-300 ease-in-out focus:scale-105 mx-auto md:mx-0"
         >
           All Archives
         </Link>
